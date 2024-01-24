5acc28b9d64a0eb3d918f6643d847a03803e03ba
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2024-01-24 09:11:02 +0000

    Added navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(config) {
+    config.addPlugin(eleventyNavigationPlugin);
     config.addPassthroughCopy('./src/assets');
     return {
         dir: {
