b4ba6710d23c514ff6864112ff37a8e55defaea2
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2024-02-10 10:22:19 +0000

    Finished footer, added dynamic copyright date

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(config) {
+    config.addShortcode("year", () => `${new Date().getFullYear()}`);
     config.addPlugin(eleventyNavigationPlugin);
     config.addPassthroughCopy('./src/assets');
     return {
